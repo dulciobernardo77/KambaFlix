923d3f6b8a0c48853cf6d701ac6a18854770bad1
dulciobernardo77 <dulciobernardo40@gmail.com>
2026-09-01 07:04:25 +0100

    first commit

---
+++ src/main/java/com/KambaFlix/KambaFlixApplication.java
@@ -0,0 +1,13 @@
+package com.KambaFlix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KambaFlixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KambaFlixApplication.class, args);
+	}
+
+}
