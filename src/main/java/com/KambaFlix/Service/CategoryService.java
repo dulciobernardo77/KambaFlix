d1ea46ef1040e21058f72f0b6661d35a89e9f686
dulciobernardo77 <dulciobernardo40@gmail.com>
2026-09-02 23:45:42 +0100

    feat: update category service

--- src/main/java/com/KambaFlix/Service/CategoryService.java
+++ src/main/java/com/KambaFlix/Service/CategoryService.java
@@ -1,7 +1,38 @@
 package com.KambaFlix.Service;
 
+import com.KambaFlix.Entity.Category;
+import com.KambaFlix.Repository.CategoryRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
-public class Category {
+public class CategoryService {
+
+    private CategoryRepository repository;
+
+    public CategoryService(CategoryRepository repository) {
+        this.repository = repository;
+    }
+
+
+    public List<Category> findAll(){
+        return repository.findAll();
+    }
+
+    public  Category cadastroDeCategory(Category category){
+        return repository.save(category);
+    }
+
+    public Category findById(Long id){
+        Optional<Category> category = repository.findById(id);
+        return category.orElse(null);
+    }
+
+    public  void delete(Long id){
+         repository.deleteById(id);
+    }
+
+
 }
