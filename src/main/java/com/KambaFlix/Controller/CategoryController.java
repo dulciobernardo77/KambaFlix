a980d562e5bcdc13399a6f368efa2859bac048b0
dulciobernardo77 <dulciobernardo40@gmail.com>
2026-09-02 23:44:12 +0100

    feat: update category controller

--- src/main/java/com/KambaFlix/Controller/CategoryController.java
+++ src/main/java/com/KambaFlix/Controller/CategoryController.java
@@ -1,4 +1,41 @@
 package com.KambaFlix.Controller;
 
+import com.KambaFlix.Entity.Category;
+import com.KambaFlix.Service.CategoryService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/kambaflix/category")
 public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService){
+        this.categoryService = categoryService;
+    }
+
+
+    @GetMapping("/lista")
+    public List<Category> getAllCategory(){
+        return categoryService.findAll();
+    }
+
+    @PostMapping("/cadastrar")
+    public Category postCadastroDeCategory(@RequestBody Category category){
+        return categoryService.cadastroDeCategory(category);
+    }
+
+    @GetMapping("/lista/{id}")
+    public Category getAllCategoryById(@PathVariable Long id){
+        return categoryService.findById(id);
+    }
+
+
+    @DeleteMapping("/lista/{id}")
+    public  void delete(@PathVariable Long id){
+         categoryService.delete(id);
+    }
+
 }
