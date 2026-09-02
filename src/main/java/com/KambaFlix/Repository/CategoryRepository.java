a6ef98e647c70dcdf5971d2fb8cf884938d68cd3
dulciobernardo77 <dulciobernardo40@gmail.com>
2026-09-02 23:45:13 +0100

    feat: update category repository

--- src/main/java/com/KambaFlix/Repository/CategoryRepository.java
+++ src/main/java/com/KambaFlix/Repository/CategoryRepository.java
@@ -1,4 +1,9 @@
 package com.KambaFlix.Repository;
 
-public interface categoryRepository {
+import com.KambaFlix.Entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category,Long> {
 }
