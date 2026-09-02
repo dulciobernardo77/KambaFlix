6478ebfa700b43a5c0fa0c260f2aeec6758dbf21
dulciobernardo77 <dulciobernardo40@gmail.com>
2026-09-02 23:41:15 +0100

    feat: add category domain files

---
+++ src/main/java/com/KambaFlix/Repository/CategoryRepository.java
@@ -0,0 +1,4 @@
+package com.KambaFlix.Repository;
+
+public interface categoryRepository {
+}
---
+++ src/main/java/com/KambaFlix/Service/CategoryService.java
@@ -0,0 +1,7 @@
+package com.KambaFlix.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Category {
+}
