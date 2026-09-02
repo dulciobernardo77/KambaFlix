adfd3ab5490d89fbcb56e64b3c1a701425435914
dulciobernardo77 <dulciobernardo40@gmail.com>
2026-09-02 23:44:42 +0100

    feat: update category entity

--- src/main/java/com/KambaFlix/Entity/Category.java
+++ src/main/java/com/KambaFlix/Entity/Category.java
@@ -1,4 +1,22 @@
 package com.KambaFlix.Entity;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "category")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "nome")
+    private  String nome ;
 }
