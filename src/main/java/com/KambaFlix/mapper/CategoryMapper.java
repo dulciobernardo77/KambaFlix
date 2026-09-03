e4a5784db02a698458c66b5924c9e8cc1a4678f1
dulciobernardo77 <dulciobernardo40@gmail.com>
2026-09-03 16:02:36 +0100

    feat: map category DTOs and entity

--- src/main/java/com/KambaFlix/mapper/CategoryMapper.java
+++ src/main/java/com/KambaFlix/mapper/CategoryMapper.java
@@ -1,4 +1,25 @@
 package com.KambaFlix.mapper;
 
+import com.KambaFlix.Controller.request.CategoryRequest;
+import com.KambaFlix.Controller.response.CategoryResponse;
+import com.KambaFlix.Entity.Category;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class CategoryMapper {
+
+    public  static  Category toCategory(CategoryRequest categoryRequest){
+        return Category
+                .builder()
+                .nome(categoryRequest.nome())
+                .build();
+    }
+
+    public static CategoryResponse toCategoryResponce(Category category){
+        return CategoryResponse
+                .builder()
+                .id(category.getId())
+                .nome(category.getNome())
+                .build();
+    }
 }
