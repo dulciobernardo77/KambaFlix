75f2a80ab0c230ebc69a1db106185f97cfc13bd2
dulciobernardo77 <dulciobernardo40@gmail.com>
2026-09-05 03:05:02 +0100

    feat(streaming): update business rules

--- src/main/java/com/KambaFlix/Service/StreamingService.java
+++ src/main/java/com/KambaFlix/Service/StreamingService.java
@@ -1,4 +1,32 @@
 package com.KambaFlix.Service;
 
+import com.KambaFlix.Entity.Streaming;
+import com.KambaFlix.Repository.StreamingRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class StreamingService {
+    private StreamingRepository repository;
+
+    public StreamingService(StreamingRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Streaming> findAll(){
+        return repository.findAll();
+    }
+
+    public  Streaming SavedCategory (Streaming streaming){
+            return repository.save(streaming);
+    }
+  public Streaming findById(Long id){
+      Optional<Streaming> streaming = repository.findById(id);
+      return streaming.orElse(null);
+  }
+  public void deleteByCategoryId (Long id){
+        repository.deleteById(id);
+  }
 }
