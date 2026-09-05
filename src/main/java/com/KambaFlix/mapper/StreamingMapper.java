c59c46c097848534220b512703d26d6482931727
dulciobernardo77 <dulciobernardo40@gmail.com>
2026-09-05 03:05:05 +0100

    feat(streaming): update data mapping

--- src/main/java/com/KambaFlix/mapper/StreamingMapper.java
+++ src/main/java/com/KambaFlix/mapper/StreamingMapper.java
@@ -1,4 +1,25 @@
 package com.KambaFlix.mapper;
 
+import com.KambaFlix.Controller.request.StreamingRequest;
+import com.KambaFlix.Controller.response.StreamingResponse;
+import com.KambaFlix.Entity.Streaming;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class StreamingMapper {
+
+    public  static Streaming toStreaming(StreamingRequest request){
+        return Streaming
+                .builder()
+                .nome(request.nome())
+                .build();
+    }
+
+    public  static StreamingResponse toStreamingResponse(Streaming streaming){
+        return StreamingResponse
+                .builder()
+                .id(streaming.getId())
+                .nome(streaming.getNome())
+                .build();
+    }
 }
