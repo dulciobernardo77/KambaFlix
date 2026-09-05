17f4e21548125c2ff04111b75dd8671a78100ddd
dulciobernardo77 <dulciobernardo40@gmail.com>
2026-09-05 03:04:58 +0100

    feat(streaming): update entity persistence

--- src/main/java/com/KambaFlix/Entity/Streaming.java
+++ src/main/java/com/KambaFlix/Entity/Streaming.java
@@ -1,4 +1,20 @@
 package com.KambaFlix.Entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@Entity
+@Table(name = "Streaming")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Streaming {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private  Long id;
+
+    @Column(name = "nome")
+    private  String nome;
 }
