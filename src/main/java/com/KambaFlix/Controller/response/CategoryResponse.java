757d879ae822fd4120249e56475800934dbcca62
dulciobernardo77 <dulciobernardo40@gmail.com>
2026-09-05 03:04:54 +0100

    fix(category): update response data

--- src/main/java/com/KambaFlix/Controller/response/CategoryResponse.java
+++ src/main/java/com/KambaFlix/Controller/response/CategoryResponse.java
@@ -3,5 +3,5 @@ package com.KambaFlix.Controller.response;
 import lombok.Builder;
 
 @Builder
-public record CategoryResponse(Long id,String nome ) {
+public record CategoryResponse(Long id,String nome) {
 }
