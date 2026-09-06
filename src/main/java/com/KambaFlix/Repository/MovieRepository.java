9e16cca293ddce305251fdc5780d7f268dd53a44
dulciobernardo77 <dulciobernardo40@gmail.com>
2026-09-06 03:38:06 +0100

    feat: implement movie service and controller

--- src/main/java/com/KambaFlix/Repository/MovieRepository.java
+++ src/main/java/com/KambaFlix/Repository/MovieRepository.java
@@ -1,4 +1,9 @@
 package com.KambaFlix.Repository;
 
-public interface MovieRepository {
+import com.KambaFlix.Entity.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Long> {
 }
--- src/main/java/com/KambaFlix/Service/MovieService.java
+++ src/main/java/com/KambaFlix/Service/MovieService.java
@@ -1,4 +1,39 @@
 package com.KambaFlix.Service;
 
+import com.KambaFlix.Entity.Movie;
+import com.KambaFlix.Repository.MovieRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class MovieService {
+
+    private final MovieRepository repository;
+
+    public MovieService(MovieRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Movie> findAll() {
+        return repository.findAll();
+    }
+     public  Movie SavedMovie(Movie movie){
+        return  repository.save(movie);
+    }
+    public Movie findById(Long id){
+     Optional<Movie> movieOptional = repository.findById(id);
+     return movieOptional.orElse(null);
+    }
+    public Movie alterByMovie(Long id,Movie movie){
+        Optional<Movie> movieOptional = repository.findById(id);
+        if (movieOptional.isPresent()){
+            return repository.save(movie);
+        }
+        return null;
+    }
+    public void deleteByCategoryId(Long id){
+        repository.deleteById(id);
+    }
 }
