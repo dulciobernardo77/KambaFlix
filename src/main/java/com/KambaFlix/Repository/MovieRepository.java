6dd69b2e00679276b3b5e2a132b83acbdf8bd5bb
dulciobernardo77 <dulciobernardo40@gmail.com>
2026-09-07 23:02:37 +0100

    feat(repository): add category-based movie lookup method

--- src/main/java/com/KambaFlix/Repository/MovieRepository.java
+++ src/main/java/com/KambaFlix/Repository/MovieRepository.java
@@ -1,9 +1,15 @@
 package com.KambaFlix.Repository;
 
+import com.KambaFlix.Entity.Category;
 import com.KambaFlix.Entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    List<Movie> findMovieByCategory(List<Category> category);
+
 }
