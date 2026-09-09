3b5ee73eff01049a542a902f2581be688727468d
dulciobernardo77 <dulciobernardo40@gmail.com>
2026-09-09 02:46:19 +0100

    feat(entity): complete user entity

--- src/main/java/com/KambaFlix/Entity/User.java
+++ src/main/java/com/KambaFlix/Entity/User.java
@@ -1,4 +1,26 @@
 package com.KambaFlix.Entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private  String  name ;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
 }
