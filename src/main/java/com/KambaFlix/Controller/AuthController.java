c5704f9fac70e33bc649a991af99c903d730f0d4
dulciobernardo77 <dulciobernardo40@gmail.com>
2026-09-09 02:45:41 +0100

    feat(controller): complete authentication controller

--- src/main/java/com/KambaFlix/Controller/AuthController.java
+++ src/main/java/com/KambaFlix/Controller/AuthController.java
@@ -1,4 +1,28 @@
 package com.KambaFlix.Controller;
 
-public class UserController {
+
+import com.KambaFlix.Controller.request.UserRequest;
+import com.KambaFlix.Controller.response.UserResponse;
+import com.KambaFlix.Entity.User;
+import com.KambaFlix.Service.UserService;
+import com.KambaFlix.mapper.UserMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/kambaflix/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final UserService userService ;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> register(@RequestBody UserRequest request){
+       User userSave= userService.save(UserMapper.toUser(request));
+       return ResponseEntity.status(HttpStatus.CREATED).body(UserMapper.toUserResponse(userSave));
+    }
 }
