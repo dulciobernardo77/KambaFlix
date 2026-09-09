3930308f43628971fed11031651e472402815c2b
dulciobernardo77 <dulciobernardo40@gmail.com>
2026-09-09 02:48:02 +0100

    feat(mapper): map user data between layers
    Adiciona a conversao entre User e os objetos de requisicao e resposta, isolando o modelo de persistencia dos contratos expostos pela API.

--- src/main/java/com/KambaFlix/mapper/UserMapper.java
+++ src/main/java/com/KambaFlix/mapper/UserMapper.java
@@ -1,4 +1,28 @@
 package com.KambaFlix.mapper;
 
+import com.KambaFlix.Controller.request.UserRequest;
+import com.KambaFlix.Controller.response.UserResponse;
+import com.KambaFlix.Entity.User;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class UserMapper {
+
+    public static User toUser(UserRequest request){
+        return User
+                .builder()
+                .name(request.name())
+                .email(request.email())
+                .password(request.password())
+                .build();
+    }
+
+    public static UserResponse toUserResponse(User user){
+        return UserResponse
+                .builder()
+                .id(user.getId())
+                .name(user.getName())
+                .email(user.getEmail())
+                .build();
+    }
 }
