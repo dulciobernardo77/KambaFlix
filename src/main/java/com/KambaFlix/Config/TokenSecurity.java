7f20cddc1f8d925ad5f9a344837875b558d58d8b
dulciobernardo77 <dulciobernardo40@gmail.com>
2026-09-21 12:10:51 +0100

    feat(security): add token security component

--- src/main/java/com/KambaFlix/Config/TokenSecurity.java
+++ src/main/java/com/KambaFlix/Config/TokenSecurity.java
@@ -1,4 +1,33 @@
 package com.KambaFlix.Config;
 
+
+import com.KambaFlix.Entity.User;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+
+@Component
 public class TokenSecurity {
+
+    @Value("${kambaflix.security.secret}")
+    private String secret;
+
+    public  String generateToken(User user){
+
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+
+        return JWT.create()
+                .withSubject(user.getEmail())
+                .withClaim("userId",user.getId())
+                .withClaim("name",user.getName())
+                .withIssuedAt(Instant.now().plusSeconds(86400))
+                .withExpiresAt(Instant.now())
+                .withIssuer("API Kambaflix")
+                .sign(algorithm);
+    }
+
 }
