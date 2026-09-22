0c9257169e141fb00362bc97dad095ee8002a729
dulciobernardo77 <dulciobernardo40@gmail.com>
2026-09-22 23:30:48 +0100

    fix: allow error dispatches in security configuration

--- src/main/java/com/KambaFlix/Config/SecurityConfig.java
+++ src/main/java/com/KambaFlix/Config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.KambaFlix.Config;
 
 
+import jakarta.servlet.DispatcherType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,6 +26,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
+                        .dispatcherTypeMatchers(DispatcherType.ERROR).permitAll()
                         .requestMatchers(HttpMethod.POST, "/kambaflix/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/kambaflix/auth/login").permitAll()
                         .anyRequest().authenticated())
