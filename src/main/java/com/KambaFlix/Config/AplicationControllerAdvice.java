15118c13e64f33fcd620e1122ba34c9cbe33ddbc
dulciobernardo77 <dulciobernardo40@gmail.com>
2026-09-22 23:29:10 +0100

    feat: add global handlers for authentication and validation errors

--- src/main/java/com/KambaFlix/Config/AplicationControllerAdvice.java
+++ src/main/java/com/KambaFlix/Config/AplicationControllerAdvice.java
@@ -1,4 +1,32 @@
 package com.KambaFlix.Config;
 
+import com.KambaFlix.Exceptions.UsenameOrPasswordInvalidExceptions;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class AplicationControllerAdvice {
+
+    @ExceptionHandler(UsenameOrPasswordInvalidExceptions.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleNotFoundException(UsenameOrPasswordInvalidExceptions ex){
+        return ex.getMessage();
+    }
+
+    
+    public Map<String, String> handleArgumentNotValideionException(MethodArgumentNotValidException ex){
+        Map<String,String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error) ->
+                errors.put(((FieldError) error).getField(), error.getDefaultMessage())
+        );
+        return  errors;
+    }
 }
